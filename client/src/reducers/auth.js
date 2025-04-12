b8c2f7f4abd56d704145ef7ee51b7955fe1a98bf
Sai Sahith <131525241+SaiSahithPolimera@users.noreply.github.com>
2025-04-12 22:02:23 +0530

    add project

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,17 @@
+const authReducer = (state = { data: null }, action) => {
+  switch (action.type) {
+    case "AUTH":
+      if (action?.data) {
+        localStorage.setItem("Profile", JSON.stringify({ ...action.data }));
+        return { ...state, data: action.data };
+      }
+      return state;
+    case "LOGOUT":
+      localStorage.clear();
+      return { ...state, data: null };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
